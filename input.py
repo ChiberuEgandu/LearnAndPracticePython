cd2139ff890d9be753a086e6e187afb8e1ddfdc8
ChiberuEgandu <95830940+ChiberuEgandu@users.noreply.github.com>
2025-09-04 19:50:21 -0400

    input and variable python files loaded  via GitHub
    input and variable python files loaded  via GitHub to test the git pull command

---
+++ input.py
@@ -0,0 +1,8 @@
+#This python file includes notes about inputs - which porompts users for an imput
+
+#input to ask user to imput or enter a name and  value
+name = input("Enter your name: ")
+value = input("Enter a value: ")
+
+print("Your name is", name , "and you entered the value ",value)
+
---
+++ variables.py
@@ -0,0 +1,26 @@
+x = 1 
+print(x)
+#X is reassigned to 2 and printed
+x = 2
+print(x)
+
+#calculations. The output for y will be 2 the last value of x + 2 
+y = x + 3
+print(y)
+
+#Created a variable called name and assigned the name "John"
+name = "John"
+print(name, "is learning about variable in python")
+
+#Reassinged the variable name to "Bob"
+name = "Bob"
+print(name, "is learning about variable in python")
+
+
+contact1 = "info@datawithbaraa.com"
+contact2 = "support@datawithbaraa.com"
+website =  "www.datawithbaraa.com"
+
+print("\n", contact1,"\n",contact2,"\n",website, "\n")
+
+
