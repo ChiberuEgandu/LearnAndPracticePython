e4b9237279113d03141bb1297a2b53b9eefc32b6
ChiberuEgandu <egandujchiberu@gmail.com>
2025-09-05 00:17:02 -0400

    Made updates to imput.py. Created a proggram that asked user for an input, their name and input value and integer and prints the results if specific paremeters are met. I used a while loop to determine if the user was entering a value/number/int

--- input.py
+++ input.py
@@ -2,7 +2,14 @@
 
 #input to ask user to imput or enter a name and  value
 name = input("Enter your name: ")
-value = input("Enter a value: ")
 
-print("Your name is", name , "and you entered the value ",value)
+while True:
+    user_input = input("Enter a value: ") # Ask the user to input a value
+    try:
+        value = int(user_input) ##The number they enter must be and integer
+        break ## If the conditions are met, kill/break out of the loop
+    except ValueError: ## If not throw this error that asked the user to try to enter another value
+        print("Error: You entered an incorrect value. Please try Again")
+
+print("Your name is ", name,  "and you entered the number:", value) ## Once they provide a name and value print this
 
