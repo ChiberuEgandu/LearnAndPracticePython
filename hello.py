18dc27d1e42b9771f694c88a93fb583d4d30b8e9
ChiberuEgandu <egandujchiberu@gmail.com>
2025-10-16 18:54:49 -0400

    New Recurstion Execisises

--- hello.py
+++
@@ -1,4 +0,0 @@
-#This is my code. This is a comment 
-print("Hello, this is my first python code Dracula Theme Soft")
-
-print(5+5)
--- operators.py
+++
@@ -1,7 +0,0 @@
-import math
-
-price = 35.545778
-
-print(round(price))
-
-print(math.floor(price))
